27618855d3435eeaf457268cc71f56d8c513fb28
kovalevcon <kovalevcon@mail.ru>
2018-09-26 13:23:06 +0600

    Added algorithm `Quick sort` for array, by first and random pivot item

---
+++ Arrays/quick-sort-random.py
@@ -0,0 +1,20 @@
+# Quick sort array with random pivot item
+# Asymptotic complexity (O) = n log n
+import random
+
+
+def quick_sort_random(arr):
+    if len(arr) < 2:
+        return arr
+    else:
+        item = random.randrange(0, len(arr))
+        pivot = arr[item]
+        arr = arr[:item] + arr[item + 1:]
+        less = [i for i in arr if i <= pivot]
+        greater = [i for i in arr if i > pivot]
+
+        print(pivot, less, greater)
+        return quick_sort_random(less) + [pivot] + quick_sort_random(greater)
+
+
+print(quick_sort_random([5, 1, 7, 2, 9, 3]))  # [1, 2, 3, 5, 7, 9]
---
+++ Arrays/quick-sort.py
@@ -0,0 +1,16 @@
+# Quick sort array
+# Asymptotic complexity (O) = n * n
+
+
+def quick_sort(arr):
+    if len(arr) < 2:
+        return arr
+    else:
+        pivot = arr[0]
+        less = [i for i in arr[1:] if i <= pivot]
+        greater = [i for i in arr[1:] if i > pivot]
+
+        return quick_sort(less) + [pivot] + quick_sort(greater)
+
+
+print(quick_sort([5, 1, 7, 2, 9, 3]))  # [1, 2, 3, 5, 7, 9]
